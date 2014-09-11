9b5d4954f5c53f512f742f6eba789822a4353082
lmsol1030 <lmsol1030@naver.com>
2014-09-11 15:54:49 +0900

    1

--- sketch.js
+++ sketch.js
@@ -5,10 +5,19 @@ function setup() {
 
   stroke(255, 0, 0);
 
+}
+
+function draw () {
+
 for (var x = 1; x <= 8; x++)
 {
   ellipse(x * 70, x * 20 + 100, x+50, x*10);
 }
+
+var size = 	Math.ads(mouseX - 320);
+
+ellipse(320, 240, );
+
 }
 
   
