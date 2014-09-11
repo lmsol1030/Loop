fadef8bc433a1fe67cb6f0ca647f6791b1e02d68
lmsol1030 <lmsol1030@naver.com>
2014-09-11 15:59:07 +0900

    4

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 function setup() {
   createCanvas(640, 480);
-  background(128, 0, 64);
+  
   noFill();
 
   stroke(255, 0, 0);
@@ -8,6 +8,8 @@ function setup() {
 }
 
 function draw () {
+
+	background(128, 0, 64);
 
 for (var x = 1; x <= 8; x++)
 {
